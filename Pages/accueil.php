f19e1a251ac13b67b3f8f8a5e18f4c5eb36e36d4
Denis Beauget <azentiaytbpro@gmail.com>
2019-10-29 18:04:50 +0100

    Ajout bouton inscription/connexion + card accueil

--- Pages/accueil.php
+++ Pages/accueil.php
@@ -1,29 +1,52 @@
 <h1 style="text-align: center">Bienvenue sur notre site ! </h1>
 <h2 style="text-align: center"> Nos Events en vedette </h2>
-<div>
-    <button style="position: absolute;" type="button" class="btn btn-primary"> Plus d'infos </button>
-    <img src="Ressources\testImg.png" class="rounded float-left img-thumbnail img-fluid">
+
+
+
+
+
+<div class="card" style="width: 18rem; display:inline-block">
+    <img src="Ressources\testImg.png" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">Titre event</h5>
+        <p class="card-text">Bref résumer.</p>
+        <a href="#" class="btn btn-primary">Plus d'infos</a>
+    </div>
 </div>
 
 
-<div>
-    <button style="position: absolute;" type="button" class="btn btn-primary"> Plus d'infos </button>
-    <img src="Ressources\testImg.png" class="rounded float-left img-thumbnail img-fluid">
+<div class="card" style="width: 18rem; display:inline-block">
+    <img src="Ressources\testImg.png" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">Titre event</h5>
+        <p class="card-text">Bref résumer.</p>
+        <a href="#" class="btn btn-primary">Plus d'infos</a>
+    </div>
+</div>
+<div class="card" style="width: 18rem; display:inline-block">
+    <img src="Ressources\testImg.png" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">Titre event</h5>
+        <p class="card-text">Bref résumer.</p>
+        <a href="#" class="btn btn-primary">Plus d'infos</a>
+    </div>
+</div>
+<div class="card" style="width: 18rem; display:inline-block">
+    <img src="Ressources\testImg.png" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">Titre event</h5>
+        <p class="card-text">Bref résumer.</p>
+        <a href="#" class="btn btn-primary">Plus d'infos</a>
+    </div>
 </div>
 
 
-<div>
-    <button style="position: absolute;" type="button" class="btn btn-primary"> Plus d'infos </button>
-    <img src="Ressources\testImg.png" class="rounded float-left img-thumbnail img-fluid">
-</div>
-
-<div>
-    <button style="position: absolute;" type="button" class="btn btn-primary"> Plus d'infos </button>
-    <img src="Ressources\testImg.png" class="rounded float-left img-thumbnail img-fluid">
-</div>
-
-
-<div>
-    <button style="position: absolute;" type="button" class="btn btn-primary"> Plus d'infos </button>
-    <img src="Ressources\testImg.png" class="rounded float-left img-thumbnail img-fluid">
-</div>
+<nav aria-label="Page navigation example" style="float:none; text-align:center; margin:0 auto;display:flex">
+    <ul class="pagination" style="float:none; text-align:center; margin:0 auto; display:flex">
+        <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+        <li class="page-item"><a class="page-link" href="#">1</a></li>
+        <li class="page-item"><a class="page-link" href="#">2</a></li>
+        <li class="page-item"><a class="page-link" href="#">3</a></li>
+        <li class="page-item"><a class="page-link" href="Pages/test.php">Next</a></li>
+    </ul>
+</nav>
--- Tools/header.php
+++ Tools/header.php
@@ -8,6 +8,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
 </head>
 
 <body>
@@ -45,10 +46,16 @@
                     </a>
                 </li>
             </ul>
+
+            <form class="form-inline" style="margin-right: 6px">
+                <button class="btn btn-outline-success" type="button" style="margin-right: 4px">S'inscrire</button>
+                <button class="btn btn-sm btn-outline-secondary" type="button">Se connecter</button>
+            </form>
             <form class="form-inline">
                 <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                <button class="btn btn-outline-success my-2 my-sm-0 " type="submit" >Rechercher</button>
             </form>
+            
         </div>
     </nav>
 
