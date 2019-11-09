b3ba2315e3a347ffd8ea4c32a374a4d6a6de6b0b
Denis <denis.beauget@etu.umontpellier.fr>
2019-11-09 13:50:11 +0100

    CreateUser

---
+++ Controllers/createUserController.php
@@ -0,0 +1,42 @@
+<?php 
+
+session_start();
+
+$dsn = 'mysql:dbname=evenements;host=127.0.0.1';
+$user = 'web';
+$password = 'ceciestunmotdepassesecurise';
+try {
+    $dbh = new PDO($dsn, $user, $password);
+} catch (PDOException $e) {
+    echo 'Connexion échouée : ' . $e->getMessage();
+}
+
+include '../Tables/visiteur.php';
+include '../Managers/visiteurManager.php';
+
+$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+if(isset($_POST['email']) && isset($_POST['prenom']) && isset($_POST['nom']) && isset($_POST['age']) && isset($_POST['pseudo']) && isset($_POST['password'])){
+    $visiteurManager = new visiteurManager($dbh);
+
+
+    $email = $_POST['email'];
+    $nom = $_POST['nom'];
+    $prenom = $_POST['prenom'];
+    $age = $_POST['age'];
+    $pseudo = $_POST['pseudo'];
+    $password = $_POST['password'];
+
+
+    $visiteur = new visiteur(0,$email,$nom,$prenom,$age,$pseudo,$password,7);
+    $visiteurManager->add($visiteur);
+
+    echo "Bravo DNS !";
+}
+
+else {
+    echo "Erreur Controller !";
+}
+
+
+?>
