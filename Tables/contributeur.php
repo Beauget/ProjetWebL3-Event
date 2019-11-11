0c35fdb43f3e8f2019fb24375623a8d2e85fcd33
sylvain courroye <sylvain.courroye@gmail.com>
2019-11-11 20:04:41 +0100

    contributeur
    Ajout de la classe contributeur

---
+++ Tables/contributeur.php
@@ -0,0 +1,90 @@
+<?php
+
+class contributeur
+{
+
+    private $_idcontrib;
+    private $_email;
+    private $_nom;
+    private $_prenom;
+    private $_age;
+    private $_pseudo;
+
+    public function __construct($idcontrib, $email, $nom, $prenom, $age, $pseudo, $args)
+    {
+        switch ($args) {
+            case 6:
+                $this->setIdContrib($idcontrib);
+                $this->setEmail($email);
+                $this->setNom($nom);
+                $this->setPrenom($prenom);
+                $this->setAge($age);
+                $this->setPseudo($pseudo);
+                break;
+            default:
+                echo "Le nombre d'argument est incorrect ! ";
+                $this->__destruct();
+        }
+    }
+
+    public function setIdContrib($idcontrib)
+    {
+        $this->_idcontrib = $idcontrib;
+    }
+
+    public function setEmail($email)
+    {
+        $this->_email = $email;
+    }
+
+    public function setNom($nom)
+    {
+        $this->_nom = $nom;
+    }
+
+    public function setPrenom($prenom)
+    {
+        $this->_prenom = $prenom;
+    }
+
+    public function setAge($age)
+    {
+        $this->_age = $age;
+    }
+
+    public function setPseudo($pseudo)
+    {
+        $this->_pseudo = $pseudo;
+    }
+
+    public function getIdContrib()
+    {
+        return this->_idcontrib;
+    }
+
+    public function getEmail()
+    {
+        return this->_email;
+    }
+
+    public function getNom()
+    {
+        return this->_nom;
+    }
+
+    public function getPrenom()
+    {
+        return this->_prenom;
+    }
+
+    public function getAge()
+    {
+        return this->_age;
+    }
+
+    public function getPseudo()
+    {
+        return this->_pseudo;
+    }
+
+}
